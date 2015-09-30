e15fa09f5ca02eeb7c7a629fbc7f3f6704707f27
Michael Swan <michael@swan.ooo>
2015-09-30 19:37:13 +0000

    Remove redundancy warnings from Main.hs.

--- src/Main.hs
+++ src/Main.hs
@@ -2,38 +2,15 @@
              ScopedTypeVariables, OverloadedStrings, FlexibleContexts #-}
 module Main where
 
-import qualified Data.ByteString.Lazy as B
-import qualified Data.Map as M
-import qualified Data.Vector as V
-import qualified Database.MySQL.Simple as MySQL
-import qualified Database.MySQL.Simple.QueryParams as MySQL
-import qualified Database.MySQL.Simple.QueryResults as MySQL
-import qualified Hasql as H
-import qualified Hasql.Postgres as HP
-import Data.Word
-import Data.Aeson
-import Data.Proxy
 import Network.Wai
 import Network.Wai.Handler.Warp
 import Network.Wai.Handler.WarpTLS
 import Data.Maybe
-import Control.Monad.Trans.Either
-import Control.Monad.Trans.Class
-import Control.Concurrent.STM.TVar
-import Control.Monad.Reader
-import Data.Text (Text)
-import Servant
-import Data.List (find)
-import Web.Cookie
-import Data.Text.Encoding
 import Data.Bool
 import Treb.Config
 import Treb.Types
 import Treb.Routes
-import Treb.Routes.Helpers
 import Treb.Routes.Types
-import Data.Time.Clock
-import Data.Functor.Identity
 
 ---- Important Functions ----
 main :: IO ()
